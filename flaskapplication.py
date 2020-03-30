4f114fbe426cc17c42456d9ac3a7ed4f79367052
kader foysal <kaderf113@gmail.com>
2020-03-30 23:28:11 +0600

    flask commit

---
+++ flaskapplication.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = '7cwtzhl0tkg9obj9'
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html', form=form, name=name)
+@app.route('/calc/<operation>/<num1>/<num2>')
+def  calc(operation,num1,num2):
+		 result = 0
+		 if operation == 'add':
+			 result = int(num1) + int(num2)
+
+		 elif operation == 'sub':
+			 result = int(num1) - int(num2)
+		 elif operation == 'mul':
+			 result = int(num1) * int(num2)
+		 elif operation == 'div':
+			 result = int(num1) / int(num2)
+		
+		 return render_template('calc.html',result=result)
+
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+if __name__ == '__main__':
+	app.run(debug=True)
